3d97cf78577568949f1d9b96efffd81b76e69b4b
jaekook <jaekook4849@gmail.com>
2021-01-08 18:10:59 +0900

    Create Thumbnail

--- server/routes/video.js
+++ server/routes/video.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const { auth } = require("../middleware/auth");
 const multer = require('multer');
+const ffmpeg = require('fluent-ffmpeg');
 
 // Multer로 서버에 비디오 저장하기
 let storage = multer.diskStorage({
@@ -50,6 +51,58 @@ router.post('/uploadfiles', (req, res) => {
             url: res.req.file.path,         // 클라이언트에게 파일 저장 위치경로를 보내줌
             fileName: res.req.file.filename
         });
+    });
+});
+
+router.post('/thumbnail', (req, res) => {
+    // 썸네일 생성하고 비디오 러닝타임(정보)도 가져오기
+
+    let filePath = "";
+    let fileDuration = "";
+    
+    // 비디오 정보 가져오기
+    ffmpeg.ffprobe(req.body.url, function(err, metadata) {
+        console.dir(metadata);
+        console.log(metadata.format.duration);
+        
+        fileDuration = metadata.format.duration;
+    });
+
+    // 썸네일 생성
+    ffmpeg(req.body.url)        // req.body.url은 client에서 온 비디오 저장 경로
+
+    // 비디오 썸네일 파일이름 생성
+    .on('filenames', function(filenames) {
+        console.log('Will generate ' + filenames.join(', '));
+        console.log(filenames);
+
+        filePath = "uploads/thumbnails/" + filenames[0];
+    })
+
+    // 썸네일 생성 후 무엇을 할 것인지
+    .on('end', function() {
+        console.log('Screenshots taken');
+        return res.json({
+            success: true,
+            url: filePath,
+            fileDuration: fileDuration
+        });
+    })
+
+    // 에러처리
+    .on('error', function(err) {
+        console.error(err);
+        return res.json({
+            success: false,
+            err
+        });
+    })
+
+    .screenshots({
+        count: 3,       // 3개의 썸네일을 찍을 수 있다
+        folder: 'uploads/thumbnails',
+        size: '320x240',
+        filename: 'thumbnail-%b.png'
     });
 });
 
