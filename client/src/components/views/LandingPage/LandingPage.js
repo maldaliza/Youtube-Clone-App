9c00e8dca7a32ae29a8bbcc854b70d0250980c92
jaekook <jaekook4849@gmail.com>
2021-01-13 16:40:45 +0900

    Add Side Video Section

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -31,27 +31,29 @@ function LandingPage() {
         var minutes = Math.floor(video.duration / 60);
         var seconds = Math.floor(video.duration - minutes * 60);
 
-        return <Col lg={6} md={8} xs={24}>
-            <div style={{ position: 'relative' }}>
-                <a href={`/video/${video._id}`}>
-                    <img style={{ width: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} />
-                    <div className="duration">
-                        <span>{minutes} : {seconds}</span>
-                    </div>
-                </a>
-            </div>
-            <br />
-            <Meta 
-                avatar={
-                    <Avatar src={video.writer.image} />
-                }
-                title={video.title}
-                description=""
-            />
-            <span>{video.writer.name}</span><br />
-            <span style={{ marginLeft: '3rem' }}>{video.views}</span>
-            - <span>{moment(video.createdAt).format("MMM Do YY")}</span>
-        </Col>;
+        return (
+            <Col lg={6} md={8} xs={24}>
+                <div style={{ position: 'relative' }}>
+                    <a href={`/video/${video._id}`}>
+                        <img style={{ width: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} />
+                        <div className="duration">
+                            <span>{minutes} : {seconds}</span>
+                        </div>
+                    </a>
+                </div>
+                <br />
+                <Meta 
+                    avatar={
+                        <Avatar src={video.writer.image} />
+                    }
+                    title={video.title}
+                    description=""
+                />
+                <span>{video.writer.name}</span><br />
+                <span style={{ marginLeft: '3rem' }}>{video.views}</span>
+                - <span>{moment(video.createdAt).format("MMM Do YY")}</span>
+            </Col>
+        );
     });
 
     return (
--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Row, Col, List, Avatar } from 'antd';
 import Axios from 'axios';
+import SideVideo from './Sections/SideVideo';
 
 function VideoDetailPage(props) {
 
@@ -26,7 +27,6 @@ function VideoDetailPage(props) {
         return (
             <Row gutter={[16, 16]}>
                 <Col lg={18} xs={24}>
-    
                     <div style={{ width: '100%', padding: '3rem 4rem' }}>
                         <video style={{ width: '100%' }} src={`http://localhost:5000/${VideoDetail.filePath}`} controls />
     
@@ -38,17 +38,15 @@ function VideoDetailPage(props) {
                                 title={VideoDetail.writer.name}
                                 description={VideoDetail.description}
                             />
-    
                         </List.Item>
     
                         {/* Comments */}
     
                     </div>
-    
-    
                 </Col>
+
                 <Col lg={6} xs={24}>
-                    Side Videos
+                    <SideVideo />
                 </Col>
             </Row>
         );
