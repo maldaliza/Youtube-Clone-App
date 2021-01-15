dda85df7362d2f0df49ea36f0ea30dd5b0047440
jaekook <jaekook4849@gmail.com>
2021-01-15 16:31:08 +0900

    Generate Subscribe Function

--- client/src/components/views/VideoDetailPage/Sections/Subscribe.js
+++ client/src/components/views/VideoDetailPage/Sections/Subscribe.js
@@ -43,15 +43,50 @@ function Subscribe(props) {
 
     }, []);
 
+
+    const onSubscribe = () => {
+
+        let subscribeVariable = {
+            userTo: props.userTo,
+            userFrom: props.userFrom
+        };
+
+        if(Subscribed) {        // 이미 구독 중인 경우
+            
+            Axios.post('/api/subscribe/unSubscribe', subscribeVariable)
+                .then(response => {
+                    if(response.data.success) {
+                        setSubscribeNumber(SubscribeNumber - 1);
+                        setSubscribed(!Subscribed);
+                    } else {
+                        alert('구독 취소하는데 실패했습니다.');
+                    }
+            });
+
+        } else {      // 아직 구독 중이 아닌 경우
+
+            Axios.post('/api/subscribe/subscribe', subscribeVariable)
+                .then(response => {
+                    if(response.data.success) {
+                        setSubscribeNumber(SubscribeNumber + 1);
+                        setSubscribed(!Subscribed);
+                    } else {
+                        alert('구독하는데 실패했습니다.');
+                    }
+            });
+        }
+    };
+
+
     return (
         <div>
             <button
                 style={{ 
-                    backgroundColor: `${Subscribe ? '#CC0000' : '#AAAAAA'}`, borderRadius: '4px',
+                    backgroundColor: `${Subscribed ? '#AAAAAA' : '#CC0000'}`, borderRadius: '4px',
                     color: 'white', padding: '10px 16px', 
                     fontWeight: '500', fontSize: '1rem', textTransform: 'uppercase'
                 }}
-                onClick
+                onClick={onSubscribe}
             >
                 {SubscribeNumber} {Subscribed ? 'Subscribed' : 'Subscribe'}
             </button>
--- server/routes/subscribe.js
+++ server/routes/subscribe.js
@@ -8,6 +8,7 @@ const { Subscriber } = require("../models/Subscriber");
 //=================================
 
 
+// 구독자 수의 정보를 가져오는 라우터
 router.post('/subscribeNumber', (req, res) => {
     Subscriber.find({ 'userTo': req.body.userTo })
     .exec((err, subscribe) => {         // subscribe에는 userTo를 구성하는 모든 케이스가 들어있다.
@@ -16,6 +17,7 @@ router.post('/subscribeNumber', (req, res) => {
     });
 });
 
+// 해당 비디오를 업로드한 유저를 구독하는지에 대한 정보를 가져오는 라우터
 router.post('/subscribed', (req, res) => {
     Subscriber.find({ 'userTo': req.body.userTo, 'userFrom': req.body.userFrom })
     .exec((err, subscribe) => {
@@ -26,6 +28,27 @@ router.post('/subscribed', (req, res) => {
             result = true;
         }
         res.status(200).json({ success: true, subscribed: result });
+    });
+});
+
+// 구독 o => 구독 x
+router.post('/unSubscribe', (req, res) => {
+    Subscriber.findOneAndDelete({ userTo: req.body.userTo, userFrom: req.body.userFrom })
+    .exec((err, doc) => {
+        if(err) return res.status(400).json({ success: false, err });
+        res.status(200).json({ success: true, doc });
+    });
+});
+
+// 구독 x => 구독 o
+router.post('/subscribe', (req, res) => {
+
+    // DB에 userTo, userFrom을 저장해야한다.
+    const subscribe = new Subscriber(req.body);
+
+    subscribe.save((err, doc) => {
+        if(err) return res.json({ success: false, err });
+        res.status(200).json({ success: true });
     });
 });
 
