c71b25d359ff5ef9127f6f344eadee891249d195
jaekook <jaekook4849@gmail.com>
2021-01-19 15:16:37 +0900

    Add Like & Dislike Click Function

--- client/src/components/views/VideoDetailPage/Sections/LikeDislikes.js
+++ client/src/components/views/VideoDetailPage/Sections/LikeDislikes.js
@@ -9,9 +9,7 @@ function LikeDislikes(props) {
     const [LikeAction, setLikeAction] = useState(null);
     const [DislikeAction, setDislikeAction] = useState(null);
 
-    let variable = {
-
-    };
+    let variable = {};
 
     if(props.video) {
         variable = { videoId: props.videoId, userId: props.userId }
@@ -60,6 +58,69 @@ function LikeDislikes(props) {
         });
     }, []);
 
+    const onLike = () => {
+
+        // 좋아요 클릭이 안되있을 때
+        if(LikeAction === null) {
+            Axios.post('/api/like/upLike', variable)
+            .then(response => {
+                if(response.data.success) {
+                    setLikes(Likes + 1);
+                    setLikeAction('liked');
+
+                    if(DislikeAction !== null) {
+                        setDislikeAction(null);
+                        setDislikes(Dislikes - 1);
+                    }
+                } else {
+                    alert('좋아요를 올리지 못했습니다.');
+                }
+            });
+        } else {        // 좋아요 클릭이 되어있을 때
+            Axios.post('/api/like/unLike', variable)
+            .then(response => {
+                if(response.data.success) {
+                    setLikes(Likes - 1);
+                    setLikeAction(null);
+                } else {
+                    alert('좋아요를 내리지 못했습니다.');
+                }
+            });
+        }
+    };
+
+    const onDislike = () => {
+
+        // 싫어요 클릭이 되어있는 경우
+        if(DislikeAction !== null) {
+            Axios.post('/api/like/unDislike', variable)
+            .then(response => {
+                if(response.data.success) {
+                    setDislikes(Dislikes - 1);
+                    setDislikeAction(null);
+                } else {
+                    alert('싫어요를 내리지 못했습니다.');
+                }
+            });
+        } else {        // 싫어요 클릭이 안되어있는 경우
+            Axios.post('/api/like/upDislike', variable)
+            .then(response => {
+                if(response.data.success) {
+                    setDislikes(Dislikes + 1);
+                    setDislikeAction('disliked');
+
+                    if(LikeAction !== null) {
+                        setLikeAction(null);
+                        setLikes(Likes - 1);
+                    }
+                } else {
+                    alert('싫어요를 올리지 못했습니다.');
+                }
+            });
+        }
+    };
+
+
     return (
         <div>
             <span key="comment-basic-like">
@@ -67,22 +128,22 @@ function LikeDislikes(props) {
                     <Icon 
                         type="like"
                         theme={LikeAction === 'liked' ? 'filled' : 'outlined'}
-                        onClick
+                        onClick={onLike}
                     />
                 </Tooltip>
                 <span style={{ paddingLeft: '8px', cursor: 'auto' }}> {Likes} </span>
-            </span>
+            </span>&nbsp;&nbsp;
 
             <span key="comment-basic-dislike">
                 <Tooltip title="Dislike">
                     <Icon 
                         type="dislike"
                         theme={DislikeAction === 'disliked' ? 'filled' : 'outlined'}
-                        onClick
+                        onClick={onDislike}
                     />
                 </Tooltip>
                 <span style={{ paddingLeft: '8px', cursor: 'auto' }}> {Dislikes} </span>
-            </span>
+            </span>&nbsp;&nbsp;
         </div>
     );
 }
--- server/routes/like.js
+++ server/routes/like.js
@@ -43,5 +43,89 @@ router.post('/getDislikes', (req, res) => {
     });
 });
 
+router.post('/upLike', (req, res) => {
+    
+    let variable = {};
+
+    if(req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId };
+    } else {
+        variable = { commentId: req.body.commentId, userId: req.body.userId };
+    }
+
+    // 1. Like collection에 클릭 정보를 넣어준다.
+    const like = new Like(variable);
+
+    like.save((err, likeResult) => {
+        if(err) return res.json({ success: false, err });
+
+        // 2. 만약 Dislike이 이미 클릭이 되있는 경우, Dislike을 1 줄여준다.
+        Dislike.findOneAndDelete(variable)
+        .exec((err, dislikeResult) => {
+            if(err) return res.status(400).json({ success: false, err });
+            res.status(200).json({ success: true });
+        });
+    });
+});
+
+router.post('/unLike', (req, res) => {
+
+    let variable = {};
+
+    if(req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId };
+    } else {
+        variable = { commentId: req.body.commentId, userId: req.body.userId };
+    }
+
+    Like.findOneAndDelete(variable)
+    .exec((err, result) => {
+        if(err) return res.status(400).json({ success: false, err });
+        res.status(200).json({ success: true });
+    });
+});
+
+router.post('/unDislike', (req, res) => {
+
+    let variable = {};
+
+    if(req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId };
+    } else {
+        variable = { commentId: req.body.commentId, userId: req.body.userId };
+    }
+
+    Dislike.findOneAndDelete(variable)
+    .exec((err, result) => {
+        if(err) return res.status(400).json({ success: false, err });
+        res.status(200).json({ success: true });
+    });
+});
+
+router.post('/upDislike', (req, res) => {
+    
+    let variable = {};
+
+    if(req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId };
+    } else {
+        variable = { commentId: req.body.commentId, userId: req.body.userId };
+    }
+
+    // 1. Dislike collection에 클릭 정보를 넣어준다.
+    const dislike = new Dislike(variable);
+
+    dislike.save((err, dislikeResult) => {
+        if(err) return res.json({ success: false, err });
+
+        // 2. 만약 Like이 이미 클릭이 되있는 경우, Like을 1 줄여준다.
+        Like.findOneAndDelete(variable)
+        .exec((err, likeResult) => {
+            if(err) return res.status(400).json({ success: false, err });
+            res.status(200).json({ success: true });
+        });
+    });
+});
+
 
 module.exports = router;
